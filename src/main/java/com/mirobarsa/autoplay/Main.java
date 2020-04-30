4c172b39cb9ec491a7c641d8b4c54339bb4174be
Miro Barsocchi <mbarsocchi@hotels.com>
2020-04-30 06:12:35 +0200

    first working jar, properties in the codebase

--- src/main/java/com/mirobarsa/autoplay/Main.java
+++ src/main/java/com/mirobarsa/autoplay/Main.java
@@ -1,25 +1,44 @@
 package com.mirobarsa.autoplay;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import static java.lang.Thread.sleep;
 import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.lang.SystemUtils;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "/Users/mbarsocchi/Selenium/chromedriver");
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String propertyPath = null;
+        if (args.length == 0) {
+            System.out.println("ERROR. No property file given");
+            propertyPath = "/Users/mbarsocchi/My_projects/rcacaster-autoplay/src/main/resources/config.properties";
+        } else {
+            propertyPath = args[0];
+        }
+
+        InputStream input = new FileInputStream(propertyPath);
+        if (input == null) {
+            System.out.println("Sorry, unable to find " + propertyPath);
+            return;
+        }
+
+        Properties prop = new Properties();
+        prop.load(input);
+
+        String casterUrl = prop.getProperty("caster.url");
+        String chromeDriver = prop.getProperty("webdriver.chrome.driver");
+        System.setProperty("webdriver.chrome.driver", chromeDriver);
         HashMap<String, Object> chromePrefs = new HashMap<>();
         ChromeOptions options = new ChromeOptions();
         options.setExperimentalOption("prefs", chromePrefs);
@@ -29,16 +48,29 @@ public class Main {
         cap.setCapability(ChromeOptions.CAPABILITY, options);
 
         WebDriver driver = new ChromeDriver(cap);
+        String cmd;
+        BrowserProcess browser = new BrowserProcess(driver, casterUrl);
         try {
-            driver.get("http://rcaradio.caster.fm/");
-            FluentWait<WebDriver> fluentWait = new FluentWait<>(driver)
-                    .pollingEvery(500, TimeUnit.MILLISECONDS)
-                    .ignoring(NoSuchElementException.class);
-            WebElement playElement = (new WebDriverWait(driver, 10))
-                    .until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class=\"jp-play\"]")));
-            playElement.click();
+            while (true) {
+                try {
+                    if (BrowserProcess.State.PLAYING != browser.getStatus()) {
+                        browser.manageNotPlaying();
+                    }
+                    sleep(4000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        } catch (Exception ex) {
         } finally {
+            if (SystemUtils.IS_OS_WINDOWS){
+                cmd ="taskkill /F /IM ChromeDriver.exe" ;
+            }else{
+                cmd ="pkill chromedriver";
+            }
+            Runtime.getRuntime().exec(cmd);
             driver.quit();
         }
+
     }
 }
